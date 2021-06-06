5629e5fa1553e9accd9a0b9ba8ee5995727801be
Sajad Rahimi <sajadrahimi6633@gmail.com>
2021-06-07 00:02:29 +0430

    add Provider package to Main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:audio_compound_effect/service/download.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
